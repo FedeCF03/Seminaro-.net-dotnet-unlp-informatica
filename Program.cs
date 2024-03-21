dd28dc4a244631d46d212e24daeb0526c6cd211d
FedeCF03 <fedecf03@gmail.com>
2024-03-20 23:14:44 -0300

    a

--- Program.cs
+++ Program.cs
@@ -48,6 +48,81 @@ pasadas como parámetros. Para el caso de la suma y la resta, las matrices deben
 en caso de no serlo devolver null. Para el caso de la multiplicación la cantidad de columnas de A debe 
 ser igual a la cantidad de filas de B, en caso contrario generar una excepción ArgumentException.
 
+double[,]? Suma(double[,] A, double[,] B)
+double[,]? Resta(double[,] A, double[,] B)
+double[,] Multiplicacion(double[,] A, double[,] B)
+
+
+double[,]? Suma(double[,] A, double[,] B)
+{
+    if (A.GetLength(0) != B.GetLength(0) || A.GetLength(1) != B.GetLength(1))
+    {
+        return null;
+    }
+    else
+    {
+        double[,] result = new double[A.GetLength(0), A.GetLength(1)];
+        for (int i = 0; i < A.GetLength(0); i++)
+        {
+            for (int j = 0; j < A.GetLength(1); j++)
+            {
+                result[i, j] = A[i, j] + B[i, j];
+            }
+        }
+        return result;
+    }
+}
+double[,]? resta(double[,] A, double[,] B)
+{
+    if (A.GetLength(0) != B.GetLength(0) || A.GetLength(1) != B.GetLength(1))
+    {
+        return null;
+    }
+    else
+    {
+        double[,] result = new double[A.GetLength(0), A.GetLength(1)];
+        for (int i = 0; i < A.GetLength(0); i++)
+        {
+            for (int j = 0; j < A.GetLength(1); j++)
+            {
+                result[i, j] = A[i, j] - B[i, j];
+            }
+        }
+        return result;
+    }
+}
+
+double[,] Multiplicacion(double[,] A, double[,] B)
+{
+    if (A.GetLength(1) != B.GetLength(0))
+    {
+        throw new ArgumentException("La cantidad de columnas de A debe ser igual a la cantidad de filas de B");
+    }
+    else
+    {
+        double[,] result = new double[A.GetLength(0), B.GetLength(1)];
+        for (int i = 0; i < A.GetLength(0); i++)
+        {
+            for (int j = 0; j < B.GetLength(1); j++)
+            {
+                for (int k = 0; k < A.GetLength(1); k++)
+                {
+                    result[i, j] += A[i, k] * B[k, j];
+                    Console.WriteLine(result[i, j]);
+                }
+
+            }
+
+
+        }
+
+        return result;
+    }
+}
+Multiplicacion(new double[,] { { 1, 25 }, { 4, 5 } }, new double[,] { { 1, 312 }, { 5, 6 } });
+
+
+
+
 
 */
-
--- obj/Debug/net8.0/teoria3.AssemblyInfo.cs
+++ obj/Debug/net8.0/teoria3.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("teoria3")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4f36eabd214f2f5e2ec98bfd1e8ec6e12821babc")]
 [assembly: System.Reflection.AssemblyProductAttribute("teoria3")]
 [assembly: System.Reflection.AssemblyTitleAttribute("teoria3")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
