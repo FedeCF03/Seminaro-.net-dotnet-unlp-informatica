b7da33d885ead0da58c5b61c0694cc9704df7669
FedeCF03 <fedecf03@gmail.com>
2024-03-22 00:02:14 -0300

    S

--- TEORIA 3/teoria3/Program.cs
+++ TEORIA 3/teoria3/Program.cs
@@ -192,5 +192,37 @@ String? DecimalABinario(int numero)
 
 Console.WriteLine(DecimalABinario(35));
 
+14) Utilizar la clase Queue<T> para implementar un programa que realice el cifrado de un texto 
+aplicando la técnica de clave repetitiva. La técnica de clave repetitiva consiste en desplazar un carácter 
+una cantidad constante de acuerdo a la lista de valores que se encuentra en la clave. Por ejemplo: para 
+la siguiente tabla de caracteres:
 
+Si la clave es 5,3,9,7 y el mensaje a cifrar es “HOLA MUNDO”, se cifra de la siguiente manera:
+A cada carácter del mensaje original se le suma la cantidad indicada en la clave. En el caso que la suma 
+fuese mayor que 28 se debe volver a contar desde el principio, Implementar una cola con los números 
+de la clave encolados y a medida que se desencolen para utilizarlos en el cifrado, se vuelvan a encolar 
+para su posterior utilización. Programar un método para cifrar y otro para descifrar
 */
+
+using System;
+
+void claveRepetida(int[] clave, string mensaje)
+{
+    Queue<int> cola = new Queue<int>();
+    int ncola = 0;
+    foreach (int c in clave)
+    {
+        cola.Enqueue(c);
+    }
+    string result = "";
+    foreach (char c in mensaje)
+    {
+
+        ncola = cola.Dequeue();
+        result += ((char)(c + ncola));
+        cola.Enqueue(ncola);
+    }
+    Console.WriteLine(result);
+}
+
+claveRepetida(new int[] { 5, 3, 9, 7 }, "HOLA MUNDO");
