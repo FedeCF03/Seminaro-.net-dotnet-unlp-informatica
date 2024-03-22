faf4ab14680a2c4b691243f6ea5bf06764655fd8
FedeCF03 <fedecf03@gmail.com>
2024-03-21 23:07:11 -0300

    s

--- TEORIA 3/teoria3/Program.cs
+++ TEORIA 3/teoria3/Program.cs
@@ -160,6 +160,37 @@ Boolean checkParentecis(String expresion)
 Console.WriteLine(checkParentecis("((1+2)*3)"));
 
 
+Utilizar la clase Stack<T> (pila) para implementar un programa 
+que pase un número en base 10 a otra base realizando divisiones 
+sucesivas. Por ejemplo para pasar 35 en base 10 a binario dividimos 
+sucesivamente por dos hasta encontrar un cociente menor que el 
+divisor, luego el resultado se obtiene leyendo de abajo hacia arriba el 
+cociente de la última división seguida por todos los restos.
+
+
+using System;
+
+String? DecimalABinario(int numero)
+{
+    string result = "";
+    Stack<int> pila = new Stack<int>();
+    while (numero >= 1)
+    {
+        pila.Push(numero % 2);
+        numero = numero / 2;
+    }
+    while (pila.Count > 0)
+    {
+        result += pila.Pop().ToString();
+    }
+    return result;
+}
+{
+
+}
+
+
+Console.WriteLine(DecimalABinario(35));
 
 
 */
